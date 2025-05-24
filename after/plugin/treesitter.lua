1f531e655c5be87fc59a90df919055d104a404cd
Nolan <nolan.king90@gmail.com>
2025-05-24 07:13:38 -0600

    treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "vim", "vimdoc", "query" , "latex", "python"},
+  ensure_installed = { "lua", "vim", "vimdoc", "query" , "python"},
   sync_install = false,
   auto_install = false,
   highlight = { enable = true },
